be7e98f98273354df985e25b0b4e97480299bb99
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-12-23 12:24:49 +0100

    Make script to extract vertex chromatic numbers of plat and arch solids

--- Code/json_to_sage.py
+++ Code/json_to_sage.py
@@ -1,18 +1,41 @@
 #!/usr/local/bin/sage -python
 
-import sage.all
+from sage.all import Graph
+from sage.graphs.graph_coloring import chromatic_number
 import json
+import os
 
-def get_solid_edges(solid_name : str):
-    with open(f"Code/JsonGraphs/Platonic/{solid_name}.json","r") as file:
+def get_solid_edges(folder_name: str, solid_filename : str):
+    with open(f"Code/JsonGraphs/{folder_name}/{solid_filename}","r") as file:
         graph_data = json.load(file)
         edges = graph_data["edges"]
         return [tuple(edge) for edge in edges]
 
+def print_chromatic_number(folder_name : str, solid_filename : str):
+    g = Graph(get_solid_edges(folder_name,solid_filename))
+    solid_name = solid_filename.split(sep=".")[0]
+    print(f"{solid_name + ":":<30}{chromatic_number(g):>5}")
+
+def process_solids(folder_name : str, solid_names : list[str]):
+    with open(f"Code/sage_graphs.txt","w") as output_file:
+        print(f"{"X(G)":-^30}")
+        for solid_filename in solid_names:
+            command_str = f"G = Graph({get_solid_edges(folder_name,solid_filename)}); G.plot(layout='planar').show(); chromatic_number(G)"
+            solid_name = solid_filename.split(sep=".")[0]
+            output_file.write(f"{solid_name}: {command_str}\n")
+            print_chromatic_number(folder_name,solid_filename)
+        print()
+
+
 solids = ["tetrahedron","cube","octahedron","dodecahedron","icosahedron"]
-with open(f"Code/sage_graphs.txt","w") as output_file:
-    for solid_name in solids:
-        command_str = f"G = Graph({get_solid_edges(solid_name)}); G.plot(layout='planar').show(); chromatic_number(G)"
-        output_file.write(f"{solid_name}: {command_str}\n")
-        print("hello")
+
+graph_jsons_path = f"{os.getcwd()}/Code/JsonGraphs"
+
+graph_jsons_folders = os.listdir(graph_jsons_path)
+
+for folder in graph_jsons_folders:
+    solid_names = os.listdir(graph_jsons_path + '/' + folder)
+    process_solids(folder,solid_names)
+
+
         
