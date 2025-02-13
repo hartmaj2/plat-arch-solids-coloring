2a17d74c8c7608ca162ca4ba5d57d1af8d96db1e
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-02-13 11:23:35 +0100

    Use more named constants

--- Code/json_to_sage.py
+++ Code/json_to_sage.py
@@ -12,46 +12,41 @@ from sage.graphs.graph_coloring import chromatic_number, edge_coloring
 import json
 import os
 
-# TABLE SIZE SETTINGS
+# TABLE VISUALS
 
 NAME_COL_SIZE : int = 30
 CHROM_NO_COL_SIZE : int = 9
 
-# TABLE HEADER NAMES
-
 VTX_CHROM_NUM_HEADER = "X(G)"
 EDG_CHROM_NUM_HEADER = "X'(G)"
+
+TABLE_COLS_SEPARATOR = "|"
+TABLE_ROWS_SEPARATOR = "="
 
 # INPUT FILE SETTINGS
 
 ROOT_FOLDER = "Code"
 INPUT_FOLDER_PATH = ROOT_FOLDER + "/JsonGraphs"
 OUTPUT_DEBUG_FILE_PATH = ROOT_FOLDER + "/sage_graphs.txt"
+JSON_EDGES_PROPERTY_KEY_NAME = "edges"
 
 def get_solid_edges(folder_name: str, solid_filename : str):
     with open(f"{INPUT_FOLDER_PATH}/{folder_name}/{solid_filename}","r") as file:
         graph_data = json.load(file)
-        edges = graph_data["edges"]
+        edges = graph_data[JSON_EDGES_PROPERTY_KEY_NAME]
         return [tuple(edge) for edge in edges]
 
 def print_chromatic_number(folder_name : str, solid_filename : str):
     g = Graph(get_solid_edges(folder_name,solid_filename))
     solid_name = solid_filename.split(sep=".")[0]
-    print(f"| {solid_name:^{NAME_COL_SIZE}} | {chromatic_number(g):^{CHROM_NO_COL_SIZE}} | {edge_coloring(g,value_only=True):^{CHROM_NO_COL_SIZE}} |")
+    print(f"{TABLE_COLS_SEPARATOR} {solid_name:^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR} {chromatic_number(g):^{CHROM_NO_COL_SIZE}} {TABLE_COLS_SEPARATOR} {edge_coloring(g,value_only=True):^{CHROM_NO_COL_SIZE}} {TABLE_COLS_SEPARATOR}")
 
 def process_solids(folder_name : str, solid_names : list[str]):
-    with open(OUTPUT_DEBUG_FILE_PATH,"w") as output_file:
-        print(f"| {folder_name:^{NAME_COL_SIZE}} | {VTX_CHROM_NUM_HEADER:^{CHROM_NO_COL_SIZE}} | {EDG_CHROM_NUM_HEADER:^{CHROM_NO_COL_SIZE}} |")
-        print(f"| {"-":-^{NAME_COL_SIZE}} | {"-":-^{CHROM_NO_COL_SIZE}} | {"-":-^{CHROM_NO_COL_SIZE}} |")
-        for solid_filename in solid_names:
-            command_str = f"G = Graph({get_solid_edges(folder_name,solid_filename)}); G.plot(layout='planar').show(); chromatic_number(G)"
-            solid_name = solid_filename.split(sep=".")[0]
-            output_file.write(f"{solid_name}: {command_str}\n")
-            print_chromatic_number(folder_name,solid_filename)
-        print()
-
-
-solids = ["tetrahedron","cube","octahedron","dodecahedron","icosahedron"]
+    print(f"{TABLE_COLS_SEPARATOR} {folder_name:^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR} {VTX_CHROM_NUM_HEADER:^{CHROM_NO_COL_SIZE}} {TABLE_COLS_SEPARATOR} {EDG_CHROM_NUM_HEADER:^{CHROM_NO_COL_SIZE}} {TABLE_COLS_SEPARATOR}")
+    print(f"{TABLE_COLS_SEPARATOR} {TABLE_ROWS_SEPARATOR:{TABLE_ROWS_SEPARATOR}^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR} {TABLE_ROWS_SEPARATOR:{TABLE_ROWS_SEPARATOR}^{CHROM_NO_COL_SIZE}} {TABLE_COLS_SEPARATOR} {TABLE_ROWS_SEPARATOR:{TABLE_ROWS_SEPARATOR}^{CHROM_NO_COL_SIZE}} {TABLE_COLS_SEPARATOR}")
+    for solid_filename in solid_names:
+        print_chromatic_number(folder_name,solid_filename)
+    print()
 
 graph_jsons_path = f"{os.getcwd()}/{INPUT_FOLDER_PATH}"
 
@@ -62,4 +57,8 @@ for folder in graph_jsons_folders:
     process_solids(folder,solid_names)
 
 
-        
+
+# SOLID NAMES
+
+# platonic_solids = ["tetrahedron","cube","octahedron","dodecahedron","icosahedron"]
+# archimedean_solids = ["truncated icosidodecahedron","truncated cube","icosidodecahedron","rhombicuboctahedron","truncated icosahedron","truncated octahedron","snub cube","truncated cuboctahedron","truncated tetrahedron","cuboctahedron","snub dodecahedron","truncated dodecahedron","rhombicosidodecahedron",] 
