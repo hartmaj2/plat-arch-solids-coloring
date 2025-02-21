b786362a1e7af00fc5761464239a3e7d721933de
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-02-21 17:59:57 +0100

    Calculate chrom polys

--- Code/chromatic_polys.py
+++ Code/chromatic_polys.py
@@ -11,30 +11,46 @@
 from sage.all import Graph
 from sage.all import *
 from sage.graphs.graph_coloring import number_of_n_colorings
-import json
-import os
+
+
+import md_table_printing as mdp
+import solids_dict_prep as sdp
 
 # INPUT FILE SETTINGS
 
 ROOT_FOLDER = "Code"
-INPUT_FOLDER_PATH = ROOT_FOLDER + "/JsonGraphs"
-POLYHEDRON_CATEGORY = "/Platonic"
-POLYHEDRON = "octahedron"
 
-k = 4
+# OUTPUT SETTING
 
-def empty_graph_k_colorings(k):
-    G = graphs.EmptyGraph()
-    vertices = 4
-    for i in range(vertices):
-        G.add_vertex()
-    G.plot().save("empty.png")
-    num_k_colorings = number_of_n_colorings(G,k)
-    print(f"empty on {vertices} vertices has {num_k_colorings} {k}-colorings")
+# uncomment following 2 lines to output to a folder
+output_file = open(ROOT_FOLDER + "/output_sage.md","w")
+output_type = output_file
 
-with open(INPUT_FOLDER_PATH + POLYHEDRON_CATEGORY + "/" + POLYHEDRON + ".json") as file:
-    graph_data = json.load(file)
-    edges = graph_data["edges"]
-    G = Graph(edges)
-    num_k_colorings = number_of_n_colorings(G,k)
-    print(f"{POLYHEDRON} has {num_k_colorings} {k}-colorings")
+# output_type = sys.stdout
+
+
+# calls sage code on given data
+def compute_chromatic_polynomial(solid_edges : list[tuple]):
+    G = Graph(solid_edges)
+    poly = G.chromatic_polynomial()
+    return poly
+
+# processes all solids and loads corresponding data to the dict
+def get_chrom_polys_dict(solid_edges : dict):
+    solid_computed_data = {}
+    for solid_name in solid_edges.keys():
+        solid_computed_data[solid_name] = str(compute_chromatic_polynomial(solid_edges[solid_name]))
+    return solid_computed_data
+
+# main loop over folders with different solid types (Platonic, Archimedean)
+def main():
+    platonic_edges = sdp.get_platonic_edges_dict()
+    platonic_data = get_chrom_polys_dict(platonic_edges)
+    mdp.print_solid_one_col_data(platonic_data,sdp.PLATONIC_FOLDER_NAME)
+    
+    archimedean_edges = sdp.get_archimedean_edges_dict()
+    archimedean_data = get_chrom_polys_dict(archimedean_edges)
+    mdp.print_solid_one_col_data(archimedean_data,sdp.ARCHIMEDEAN_FOLDER_NAME)
+
+if __name__ == "__main__": # __name__ variable is either `__main__` or `json_to_sage`
+    main()
--- Code/md_table_printing.py
+++ Code/md_table_printing.py
@@ -25,3 +25,15 @@ def print_solid_chrom_nums(solid_data_dict : dict, solid_category_name = "solid"
         table_entry_row = f"{TABLE_COLS_SEPARATOR} {solid_name:^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR} {vtx_chrom_num:^{CHROM_NO_COL_SIZE}} {TABLE_COLS_SEPARATOR} {edge_chrom_num:^{CHROM_NO_COL_SIZE}} {TABLE_COLS_SEPARATOR}"
         print(table_entry_row,file=output_type)
     print(file=output_type)
+
+# prints md table given the dictionary of data for the given solid
+def print_solid_one_col_data(solid_data_dict: dict, solid_category_name = "solid", data_col_name = "data", data_col_size = 9, output_type = sys.stdout):
+    header_line_string = f"{TABLE_COLS_SEPARATOR} {solid_category_name:^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR} {data_col_name:^{data_col_size}} {TABLE_COLS_SEPARATOR}"
+    header_separator_string = f"{TABLE_COLS_SEPARATOR} {TABLE_ROWS_SEPARATOR:{TABLE_ROWS_SEPARATOR}^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR}:{TABLE_ROWS_SEPARATOR:{TABLE_ROWS_SEPARATOR}^{data_col_size}}:{TABLE_COLS_SEPARATOR}"
+    print(header_line_string,file=output_type)
+    print(header_separator_string,file=output_type)
+    for solid_name in sorted(solid_data_dict.keys()):
+        solid_data = solid_data_dict[solid_name]
+        table_entry_row = f"{TABLE_COLS_SEPARATOR} {solid_name:^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR} {solid_data:^{data_col_size}} {TABLE_COLS_SEPARATOR}"
+        print(table_entry_row,file=output_type)
+    print(file=output_type)
