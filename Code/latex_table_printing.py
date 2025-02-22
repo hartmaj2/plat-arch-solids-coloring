f8101b0fad83322a210d780ae5d2c39dca0084c5
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-02-22 12:47:51 +0100

    Add generalized latex table printing function

--- Code/latex_table_printing.py
+++ Code/latex_table_printing.py
@@ -1,3 +1,7 @@
+# IMPORTS 
+
+import sys
+
 # TABLE VISUALS
 
 NAME_COL_SIZE : int = 30
@@ -26,7 +30,7 @@ PLATONIC_FOLDER_NAME = "Platonic"
 PLATONIC_OUTPUT_NAMES = ["tetrahedron","cube","octahedron","dodecahedron","icosahedron"]
 ARCHIMEDEAN_OUTPUT_NAMES = ["truncated icosidodecahedron","truncated cube","icosidodecahedron","rhombicuboctahedron","truncated icosahedron","truncated octahedron","snub cube","truncated cuboctahedron","truncated tetrahedron","cuboctahedron","snub dodecahedron","truncated dodecahedron","rhombicosidodecahedron",]
 
-def print_solids(solid_dict : dict, solid_category : str, output_type):
+def print_solids(solid_dict : dict, solid_category : str, output_type = sys.stdout):
     tabular_format_string = f"{TABULAR_BEGIN}{CURLY_BRACE_LEFT}{TABLE_VERTICAL_LINE_MARKER}{ALIGNMENT_CHAR_LEFT}{TABLE_VERTICAL_LINE_MARKER}{ALIGNMENT_CHAR_CENTER}{TABLE_VERTICAL_LINE_MARKER}{ALIGNMENT_CHAR_CENTER}{TABLE_VERTICAL_LINE_MARKER}{CURLY_BRACE_RIGHT}"
     print(tabular_format_string,file=output_type)
     print(LATEX_HORIZONTAL_LINE,file=output_type)
@@ -36,6 +40,22 @@ def print_solids(solid_dict : dict, solid_category : str, output_type):
     for solid_name in sorted(solid_dict.keys()):
         vtx_chrom_num, edge_chrom_num = solid_dict[solid_name]
         table_entry_row = f"{solid_name} {LATEX_COL_SEPARATOR} {vtx_chrom_num} {LATEX_COL_SEPARATOR} {edge_chrom_num} {LATEX_NEWLINE}"
+        print(table_entry_row,file=output_type)
+        print(LATEX_HORIZONTAL_LINE,file=output_type)
+    print(TABULAR_END,file=output_type)
+
+
+# prints latex table given the dictionary of data for the given solid
+def print_solid_one_col_data(solid_data_dict: dict, solid_category_name = "solid", data_col_name = "data", output_type = sys.stdout):
+    tabular_format_string = f"{TABULAR_BEGIN}{CURLY_BRACE_LEFT}{TABLE_VERTICAL_LINE_MARKER}{ALIGNMENT_CHAR_LEFT}{TABLE_VERTICAL_LINE_MARKER}{ALIGNMENT_CHAR_CENTER}{TABLE_VERTICAL_LINE_MARKER}{CURLY_BRACE_RIGHT}"
+    print(tabular_format_string,file=output_type)
+    print(LATEX_HORIZONTAL_LINE,file=output_type)
+    header_line_string = f"{solid_category_name} {LATEX_COL_SEPARATOR} {str(data_col_name)} {LATEX_NEWLINE}"
+    print(header_line_string,file=output_type)
+    print(LATEX_HORIZONTAL_LINE + LATEX_HORIZONTAL_LINE,file=output_type)
+    for solid_name in sorted(solid_data_dict.keys()):
+        solid_data = str(solid_data_dict[solid_name])
+        table_entry_row = f"{solid_name} {LATEX_COL_SEPARATOR} {solid_data} {LATEX_NEWLINE}"
         print(table_entry_row,file=output_type)
         print(LATEX_HORIZONTAL_LINE,file=output_type)
     print(TABULAR_END,file=output_type)
