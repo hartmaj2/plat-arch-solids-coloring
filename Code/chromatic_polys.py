804f135b22e807aa1c57dbfcacaba475c731704f
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-02-22 12:47:20 +0100

    Added code to skip intractable computations

--- Code/chromatic_polys.py
+++ Code/chromatic_polys.py
@@ -7,7 +7,7 @@
 from sage.all import Graph
 from sage.all import *
 
-import md_table_printing as mdp
+import latex_table_printing as printing
 import solids_dict_prep as sdp
 
 # INPUT FILE SETTINGS
@@ -20,8 +20,12 @@ ROOT_FOLDER = "Code"
 output_file = open(ROOT_FOLDER + "/output_sage.md","w")
 output_type = output_file
 
-import sys
-output_type = sys.stdout
+# import sys
+# output_type = sys.stdout
+
+# DEBUG
+
+polys_to_skip = ["truncated icosidodecahedron","icosidodecahedron","rhombicuboctahedron","truncated icosahedron","snub cube","truncated cuboctahedron","snub dodecahedron","truncated dodecahedron","rhombicosidodecahedron"]
 
 
 # calls sage code on given data
@@ -34,18 +38,23 @@ def compute_chromatic_polynomial(solid_edges : list[tuple]):
 def get_chrom_polys_dict(solid_edges : dict):
     solid_computed_data = {}
     for solid_name in solid_edges.keys():
-        solid_computed_data[solid_name] = str(compute_chromatic_polynomial(solid_edges[solid_name]))
+        if solid_name in polys_to_skip:
+            solid_computed_data[solid_name] = None
+            print(f"skipping chromial of {solid_name}")
+        else:
+            print(f"calculating chromial of {solid_name}")
+            solid_computed_data[solid_name] = compute_chromatic_polynomial(solid_edges[solid_name])
     return solid_computed_data
 
 # main loop over folders with different solid types (Platonic, Archimedean)
 def main():
     platonic_edges = sdp.get_platonic_edges_dict()
     platonic_data = get_chrom_polys_dict(platonic_edges)
-    mdp.print_solid_one_col_data(platonic_data,sdp.PLATONIC_FOLDER_NAME,output_type=output_type)
+    printing.print_solid_one_col_data(platonic_data,sdp.PLATONIC_FOLDER_NAME,output_type=output_type)
     
     archimedean_edges = sdp.get_archimedean_edges_dict()
     archimedean_data = get_chrom_polys_dict(archimedean_edges)
-    mdp.print_solid_one_col_data(archimedean_data,sdp.ARCHIMEDEAN_FOLDER_NAME,output_type=output_type)
+    printing.print_solid_one_col_data(archimedean_data,sdp.ARCHIMEDEAN_FOLDER_NAME,output_type=output_type)
 
 if __name__ == "__main__": # __name__ variable is either `__main__` or `json_to_sage`
     main()
