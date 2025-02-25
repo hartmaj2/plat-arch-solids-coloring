108bc0ab6129662509a2820fdd8919edbc5c62dc
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-02-25 21:01:09 +0100

    Add function to print multiple column data

--- Code/md_table_printing.py
+++ Code/md_table_printing.py
@@ -37,3 +37,20 @@ def print_solid_one_col_data(solid_data_dict: dict, solid_category_name = "solid
         table_entry_row = f"{TABLE_COLS_SEPARATOR} {solid_name:^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR} {solid_data:^{data_col_size}} {TABLE_COLS_SEPARATOR}"
         print(table_entry_row,file=output_type)
     print(file=output_type)
+
+# prints md table given the dictionary of data, where the dictionary points from name -> [data1, ... , dataN]
+def print_solid_mult_col_data(solid_data_dict: dict, solid_category_name : str, data_col_headrs : list[str], data_col_size = 9, output_type = sys.stdout):
+    header_line_string = f"{TABLE_COLS_SEPARATOR} {solid_category_name:^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR}"
+    header_separator_string = f"{TABLE_COLS_SEPARATOR} {TABLE_ROWS_SEPARATOR:{TABLE_ROWS_SEPARATOR}^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR}"
+    for data_col_headr in data_col_headrs:
+        header_line_string += f" {data_col_headr:^{data_col_size}} {TABLE_COLS_SEPARATOR}"
+        header_separator_string += f":{TABLE_ROWS_SEPARATOR:{TABLE_ROWS_SEPARATOR}^{data_col_size}}:{TABLE_COLS_SEPARATOR}"
+    print(header_line_string,file=output_type)
+    print(header_separator_string,file=output_type)
+    for solid_name in sorted(solid_data_dict.keys()):
+        solid_data_cols = solid_data_dict[solid_name]
+        table_entry_row = f"{TABLE_COLS_SEPARATOR} {solid_name:^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR}"
+        for solid_data_entry in solid_data_cols:
+            table_entry_row += f" {solid_data_entry:^{data_col_size}} {TABLE_COLS_SEPARATOR}"
+        print(table_entry_row,file=output_type)
+    print(file=output_type)
