3d18d0e826c9b807eb3961adee31ea858bc1063e
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-03-02 17:19:01 +0100

    Make use of solid dict instead of edges dict

--- Code/compute_chrom_nums.py
+++ Code/compute_chrom_nums.py
@@ -15,6 +15,10 @@ from sage.graphs.graph_coloring import chromatic_number, edge_coloring
 import md_table_printing as printing
 import solids_dict_prep as sdp
 
+VERTICES = sdp.JSON_VERTICES
+EDGES = sdp.JSON_EDGES
+NAME = sdp.JSON_NAME
+
 # INPUT FILE SETTINGS
 
 ROOT_FOLDER = "Code"
@@ -35,19 +39,19 @@ def calculate_chromatic_numbers(solid_edges : list[tuple]) -> tuple[int,int] :
     return vtx_chrom_num, edge_chrom_num
 
 # processes all solids and loads corresponding data to the dict
-def get_chrom_nums_dict(solid_edges : dict):
+def get_chrom_nums_dict(solid_data : dict[dict]):
     solid_computed_data = {}
-    for solid_name in solid_edges.keys():
-        solid_computed_data[solid_name] = calculate_chromatic_numbers(solid_edges[solid_name])
+    for solid_name in solid_data.keys():
+        solid_computed_data[solid_name] = calculate_chromatic_numbers(solid_data[solid_name][EDGES])
     return solid_computed_data
 
 # main loop over folders with different solid types (Platonic, Archimedean)
 def main():
-    platonic_edges = sdp.get_platonic_edges_dict()
+    platonic_edges = sdp.get_platonic_solid_dict()
     platonic_data = get_chrom_nums_dict(platonic_edges)
     printing.print_solid_chrom_nums(platonic_data,sdp.PLATONIC_FOLDER)
     
-    archimedean_edges = sdp.get_archimedean_edges_dict()
+    archimedean_edges = sdp.get_archimedean_solid_dict()
     archimedean_data = get_chrom_nums_dict(archimedean_edges)
     printing.print_solid_chrom_nums(archimedean_data,sdp.ARCHIMEDEAN_FOLDER)
 
