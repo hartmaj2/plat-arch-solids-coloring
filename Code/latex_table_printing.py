95928ebfc7edd99de8372877212aa28b6c38ef4e
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-03-03 11:31:57 +0100

    Prune unnecessary printing code

--- Code/latex_table_printing.py
+++ Code/latex_table_printing.py
@@ -5,9 +5,6 @@ import re
 
 # TABLE VISUALS
 
-NAME_COL_SIZE : int = 30
-CHROM_NO_COL_SIZE : int = 9
-
 TABLE_BEGIN_HERE = r"\begin{table}[H]"
 TABLE_END = r"\end{table}"
 CENTERING = r"\centering"
@@ -17,9 +14,6 @@ LABEL_PLACEHOLDER = "tab:enter-custom-label-here"
 
 TABULAR_BEGIN = r"\begin{tabular}"
 TABULAR_END = r"\end{tabular}"
-
-VTX_CHROM_NUM_HEADER = r"$\chi(G)$"
-EDG_CHROM_NUM_HEADER = r"$\chi'(G)$"
 
 TABLE_VERTICAL_LINE_MARKER = '|'
 ALIGNMENT_CHAR_CENTER = 'c'
@@ -32,36 +26,8 @@ LATEX_COL_SEPARATOR = '&'
 CURLY_BRACE_LEFT = r"{"
 CURLY_BRACE_RIGHT = r"}"
 
-ARCHIMEDEAN_FOLDER_NAME = "Archimedean"
-PLATONIC_FOLDER_NAME = "Platonic"
-
 PLATONIC_OUTPUT_NAMES = ["tetrahedron","cube","octahedron","dodecahedron","icosahedron"]
-ARCHIMEDEAN_OUTPUT_NAMES = ["truncated icosidodecahedron","truncated cube","icosidodecahedron","rhombicuboctahedron","truncated icosahedron","truncated octahedron","snub cube","truncated cuboctahedron","truncated tetrahedron","cuboctahedron","snub dodecahedron","truncated dodecahedron","rhombicosidodecahedron",]
-
-def print_solids(solid_dict : dict, solid_category : str, output_type = sys.stdout):
-    tabular_format_string = f"{TABULAR_BEGIN}{CURLY_BRACE_LEFT}{TABLE_VERTICAL_LINE_MARKER}{ALIGNMENT_CHAR_LEFT}{TABLE_VERTICAL_LINE_MARKER}{ALIGNMENT_CHAR_CENTER}{TABLE_VERTICAL_LINE_MARKER}{ALIGNMENT_CHAR_CENTER}{TABLE_VERTICAL_LINE_MARKER}{CURLY_BRACE_RIGHT}"
-    print(TABLE_BEGIN_HERE,file=output_type)
-    print(CENTERING,file=output_type)
-    print(r"\caption{",file=output_type,end="")
-    print(CAPTION_PLACEHOLDER,file=output_type,end="")
-    print(r"}",file=output_type)
-    print(VERTICAL_SPACE,file=output_type)
-    print()
-    print(r"\label{",file=output_type,end="")
-    print(LABEL_PLACEHOLDER,file=output_type,end="")
-    print(r"}",file=output_type)
-    print(tabular_format_string,file=output_type)
-    print(LATEX_HORIZONTAL_LINE,file=output_type)
-    header_line_string = f"{solid_category} {LATEX_COL_SEPARATOR} {VTX_CHROM_NUM_HEADER} {LATEX_COL_SEPARATOR} {EDG_CHROM_NUM_HEADER} {LATEX_NEWLINE}"
-    print(header_line_string,file=output_type)
-    print(LATEX_HORIZONTAL_LINE + LATEX_HORIZONTAL_LINE,file=output_type)
-    for solid_name in sorted(solid_dict.keys()):
-        vtx_chrom_num, edge_chrom_num = solid_dict[solid_name]
-        table_entry_row = f"{solid_name} {LATEX_COL_SEPARATOR} {vtx_chrom_num} {LATEX_COL_SEPARATOR} {edge_chrom_num} {LATEX_NEWLINE}"
-        print(table_entry_row,file=output_type)
-        print(LATEX_HORIZONTAL_LINE,file=output_type)
-    print(TABULAR_END,file=output_type)
-    print(TABLE_END,file=output_type)
+ARCHIMEDEAN_OUTPUT_NAMES = ["truncated icosidodecahedron","truncated cube","icosidodecahedron","rhombicuboctahedron","truncated icosahedron","truncated octahedron","snub cube","truncated cuboctahedron","truncated tetrahedron","cuboctahedron","snub dodecahedron","truncated dodecahedron","rhombicosidodecahedron"]
 
 # prints latex table given the dictionary of data for the given solid
 def print_solid_one_col_data(solid_data_dict: dict, solid_category_name = "solid", data_col_name = "data", data_alignment = ALIGNMENT_CHAR_CENTER, output_type = sys.stdout, transform = lambda x : str(x)):
--- Code/md_table_printing.py
+++ Code/md_table_printing.py
@@ -5,28 +5,12 @@ import sys
 # TABLE VISUALS
 
 NAME_COL_SIZE : int = 40
-CHROM_NO_COL_SIZE : int = 9
+DEFAULT_COL_SIZE : int = 9
 
 CAPTION_PLACEHOLDER = "Enter caption here."
 
-VTX_CHROM_NUM_HEADER = "X(G)"
-EDG_CHROM_NUM_HEADER = "X'(G)"
-
 TABLE_COLS_SEPARATOR = "|"
 TABLE_ROWS_SEPARATOR = "-"
-
-# prints a table given the solids in solid_data_dict and the name of the solid category
-# prints to the output given by third parameter
-def print_solid_chrom_nums(solid_data_dict : dict, solid_category_name = "solid", output_type = sys.stdout):
-    header_line_string = f"{TABLE_COLS_SEPARATOR} {solid_category_name:^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR} {VTX_CHROM_NUM_HEADER:^{CHROM_NO_COL_SIZE}} {TABLE_COLS_SEPARATOR} {EDG_CHROM_NUM_HEADER:^{CHROM_NO_COL_SIZE}} {TABLE_COLS_SEPARATOR}"
-    header_separator_string = f"{TABLE_COLS_SEPARATOR} {TABLE_ROWS_SEPARATOR:{TABLE_ROWS_SEPARATOR}^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR}:{TABLE_ROWS_SEPARATOR:{TABLE_ROWS_SEPARATOR}^{CHROM_NO_COL_SIZE}}:{TABLE_COLS_SEPARATOR}:{TABLE_ROWS_SEPARATOR:{TABLE_ROWS_SEPARATOR}^{CHROM_NO_COL_SIZE}}:{TABLE_COLS_SEPARATOR}"
-    print(header_line_string,file=output_type)
-    print(header_separator_string,file=output_type)
-    for solid_name in sorted(solid_data_dict.keys()):
-        vtx_chrom_num, edge_chrom_num = solid_data_dict[solid_name]
-        table_entry_row = f"{TABLE_COLS_SEPARATOR} {solid_name:^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR} {vtx_chrom_num:^{CHROM_NO_COL_SIZE}} {TABLE_COLS_SEPARATOR} {edge_chrom_num:^{CHROM_NO_COL_SIZE}} {TABLE_COLS_SEPARATOR}"
-        print(table_entry_row,file=output_type)
-    print(file=output_type)
 
 # prints md table given the dictionary of data for the given solid
 def print_solid_one_col_data(solid_data_dict: dict, solid_category_name = "solid", data_col_name = "data", data_col_size = 9, output_type = sys.stdout):
@@ -42,7 +26,7 @@ def print_solid_one_col_data(solid_data_dict: dict, solid_category_name = "solid
 
 # prints md table given the dictionary of data, where the dictionary points from name -> [data1, ... , dataN]
 # on input we expect list of header names passed to `data_col_headrs`
-def print_solid_mult_col_data(solid_data_dict: dict, solid_category_name : str, data_col_headrs : list[str], data_col_size = 9, caption = CAPTION_PLACEHOLDER, label = "",output_type = sys.stdout):
+def print_solid_mult_col_data(solid_data_dict: dict, solid_category_name : str, data_col_headrs : list[str], data_col_size = DEFAULT_COL_SIZE, caption = CAPTION_PLACEHOLDER, label = "",output_type = sys.stdout):
     print(caption,file=output_type)
     header_line_string = f"{TABLE_COLS_SEPARATOR} {solid_category_name:^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR}"
     header_separator_string = f"{TABLE_COLS_SEPARATOR} {TABLE_ROWS_SEPARATOR:{TABLE_ROWS_SEPARATOR}^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR}"
