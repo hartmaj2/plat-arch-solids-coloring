0f345282df355a2bdc48f4187a5c77bc4d0c06ab
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-03-13 17:47:50 +0100

    Implement function to calculate orbital chromatic polynomial of a graph

---
+++ Code/graph_utils/orbital_chrompoly.py
@@ -0,0 +1,28 @@
+from sage.all import Graph
+from sage.all import PolynomialRing, ZZ
+
+def orbital_chromatic_polynomial(g : Graph):  
+
+    R = PolynomialRing(ZZ, 'x') # create a polynomial ring of integers where the resulting polynomial will live
+    p = R(0)
+
+    A = g.automorphism_group()
+
+    for a in A:
+        cycles = a.cycle_tuples()
+        valid_cycles = True
+        merged = g.copy()
+        for c in cycles:
+            if not g.is_independent_set(c): # if any two vertices on a cycle are connected by an edge in the original graph, then this automorphism has empty fixed point
+                valid_cycles = False
+                break
+            merged.merge_vertices(c) # all the vertices on the cycle must have same color so we can simulate this by merging them
+        if not valid_cycles:
+            continue
+        p += merged.chromatic_polynomial()
+
+    p = p / A.order()
+    return p
+    
+
+
