9ca7a6c64cebb733e05bf457909c5deb56657cda
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-03-13 17:48:08 +0100

    Add orital chrompoly function to script

--- Code/single_chrom_poly.py
+++ Code/single_chrom_poly.py
@@ -8,6 +8,7 @@ from sage.all import Graph
 from sage.all import *
 
 import solids_prep.solids_dict_prep as sdp
+import graph_utils.orbital_chrompoly as orbchrom
 import time
 
 # DEBUG
@@ -21,16 +22,20 @@ def compute_chromatic_polynomial(solid_edges : list[tuple]):
     poly = G.chromatic_polynomial()
     return poly
 
+def compute_orbital_chromatic_polynomial(solid_edges : list[tuple]):
+    G = Graph(solid_edges)
+    poly = orbchrom.orbital_chromatic_polynomial(G)
+    return poly
 
 # main loop over folders with different solid types (Platonic, Archimedean)
 def main():
-    solid_name = "snub cube"
-    archimedean_edges = sdp.get_archimedean_edges_dict()
-    edges = archimedean_edges[solid_name]
+    solid_name = "cube"
+    solid_dict = sdp.get_all_solids_dict()[solid_name]
+    edges = solid_dict[sdp.JSON_EDGES]
     print(solid_name)
     starting_time = time.time()
     print(f"{starting_time=}")
-    print(str(compute_chromatic_polynomial(edges)))
+    print(str(compute_orbital_chromatic_polynomial(edges)))
     end_time = time.time()
     duration = end_time - starting_time
     print(f"{end_time=}")
