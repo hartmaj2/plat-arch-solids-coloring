56d9e2a4cc4622687e854854d95240ef827c3930
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-03-20 16:10:15 +0100

    Use automorphism table printer to print latex table

---
+++ Code/Results/automorphisms.md
@@ -0,0 +1,42 @@
+\begin{table}[H]
+\centering
+\begin{tabular}{l@{\hspace{1.5cm}}c}
+\toprule
+\textbf{Platonic} & \textbf{$\abs{\Aut(G)}$} \\
+\midrule
+cube & 48 \\
+dodecahedron & 120 \\
+icosahedron & 120 \\
+octahedron & 48 \\
+tetrahedron & 24 \\
+\bottomrule
+\end{tabular}
+\caption{Sizes of automorphism groups of graphs of Platonic solids.}
+\label{tab:plat-automorphisms}
+\end{table}
+
+\begin{table}[H]
+\centering
+\begin{tabular}{l@{\hspace{1.5cm}}c}
+\toprule
+\textbf{Archimedean} & \textbf{$\abs{\Aut(G)}$} \\
+\midrule
+cuboctahedron & 48 \\
+icosidodecahedron & 120 \\
+rhombicosidodecahedron & 120 \\
+rhombicuboctahedron & 48 \\
+snub cube & 24 \\
+snub dodecahedron & 60 \\
+truncated cube & 48 \\
+truncated cuboctahedron & 48 \\
+truncated dodecahedron & 120 \\
+truncated icosahedron & 120 \\
+truncated icosidodecahedron & 120 \\
+truncated octahedron & 48 \\
+truncated tetrahedron & 24 \\
+\bottomrule
+\end{tabular}
+\caption{Sizes of automorphism groups of graphs of Archimedean solids.}
+\label{tab:arch-automorphisms}
+\end{table}
+
--- Code/automorphisms.py
+++ Code/automorphisms.py
@@ -3,7 +3,7 @@
 # Prints a table that shows how many automorphisms the platonic and archimedean solids have
 
 import solids_prep.solids_dict_prep as sdp
-import t_printing.md_table_printing as printing
+import t_printing.latex_table_printing as printing
 
 from sage.all import Graph
 
@@ -19,9 +19,23 @@ archimedean = sdp.get_archimedean_solid_dict()
 plat_data = {}
 arch_data = {}
 
-PLATONIC_HEADER = "platonic"
-ARCHIMEDEAN_HEADER = "archimedean"
-COLUMN_HEADERS = ["automorphisms"]
+PLATONIC_HEADER = "Platonic"
+PLATONIC_CAPTION = "Sizes of automorphism groups of graphs of Platonic solids."
+PLATONIC_LABEL = "tab:plat-automorphisms"
+
+ARCHIMEDEAN_HEADER = "Archimedean"
+ARCHIMEDEAN_CAPTION = "Sizes of automorphism groups of graphs of Archimedean solids."
+ARCHIMEDEAN_LABEL = "tab:arch-automorphisms"
+
+# OUTPUT TABLE HEADERS
+LATEX_COL_HEADERS = [r"$\abs{\Aut(G)}$"]
+MD_COL_HEADERS = ["automorphisms"]
+column_headers = LATEX_COL_HEADERS
+
+# OUTPUT FOLDER 
+ROOT_FOLDER = "Code"
+output_file = open(ROOT_FOLDER + "/Results/automorphisms.md","w")
+output_type = output_file
 
 COL_SIZE = 20
 
@@ -44,8 +58,8 @@ for solid_name in archimedean.keys():
     arch_data[solid_name] = [order]
     # print(f"{solid_name:30} {G.order()}") 
 
-printing.print_solid_mult_col_data(plat_data,PLATONIC_HEADER,COLUMN_HEADERS,COL_SIZE)
-printing.print_solid_mult_col_data(arch_data,ARCHIMEDEAN_HEADER,COLUMN_HEADERS,COL_SIZE)
+printing.print_solid_mult_col_data(plat_data,PLATONIC_HEADER,column_headers,PLATONIC_CAPTION,PLATONIC_LABEL,output_type)
+printing.print_solid_mult_col_data(arch_data,ARCHIMEDEAN_HEADER,column_headers,ARCHIMEDEAN_CAPTION,ARCHIMEDEAN_LABEL,output_type)
 
 # print(f"{platonic[solid_key]}")
 
