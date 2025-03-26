3f74b737cead58db6639615d005f0731ba21ab86
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-03-26 21:14:00 +0100

    Add more versions of orbital chrom polynomial computing function

--- Code/graph_utils/orbital_chrompoly.py
+++ Code/graph_utils/orbital_chrompoly.py
@@ -27,5 +27,36 @@ def orbital_chromatic_polynomial(g : Graph):
     p = p / A.order()
     return p
     
+# version with for-else control flow structure
+def orbital_chromatic_polynomial2(g : Graph):  
+    R = PolynomialRing(ZZ, 'x') # create a polynomial ring of integers where the resulting polynomial will live
+    p = R(0)
+    A = g.automorphism_group()
+    for a in A:
+        cycles = a.cycle_tuples()
+        merged = g.copy()
+        for c in cycles:
+            if not g.is_independent_set(c): # if any two vertices on a cycle are connected by an edge in the original graph, then this automorphism has empty fixed point
+                break
+            merged.merge_vertices(c) # all the vertices on the cycle must have same color so we can simulate this by merging them
+        else:
+            p += merged.chromatic_polynomial()
+    p = p / A.order()
+    return p
+    
 
-
+# less verbose version of the algorithm
+def orbital_chromatic_polynomial3(g : Graph):  
+    R = PolynomialRing(ZZ, 'x') # create a polynomial ring of integers where the resulting polynomial will live
+    p = R(0)
+    A = g.automorphism_group()
+    for a in A:
+        cycles = a.cycle_tuples()
+        if not all([g.is_independent_set(c) for c in cycles]): # if for some cycle, any two vertices are connected by an edge in the original graph, then no coloring can be fixed
+            continue
+        merged = g.copy()
+        for c in cycles:
+            merged.merge_vertices(c) # all the vertices on the cycle must have same color so we can simulate this by merging them
+        p += merged.chromatic_polynomial()
+    p = p / A.order()
+    return p
