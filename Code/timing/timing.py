e40128e7e8562da4c13a77a59a7f250a6b14de30
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-04-02 21:42:50 +0200

    Implement module for benchmarking functions

--- Code/timing/timing.py
+++ Code/timing/timing.py
@@ -1,18 +1,7 @@
 import time
 
-HEADER = "TIMING:"
-UNDERLINE_MARKER = "-"
-
-# *tup packs the tuple of non-keyworded arguments into a tuple and names it `tup`
-# **dic pack the tuple of keyworded arguments into a dict and names it `dic`
-def run_timed(func, *args, **kwargs):
+# run the function with the given non-keyword and keyword arguments and return the result and the run time as well
+def run_and_get_time(func, *args, **kwargs):
     start = time.time()
-    res = func(*args, **kwargs) # * unpacs the tuple and ** unpacs the dict
-    function = func.__name__
-    end = time.time()
-    duration = end - start
-    print(HEADER)
-    print("".join([UNDERLINE_MARKER for letter in HEADER]))
-    print(f"{function=}\n{duration=}")
-    return res
-    
+    res = func(*args,**kwargs)
+    return res, time.time() - start
