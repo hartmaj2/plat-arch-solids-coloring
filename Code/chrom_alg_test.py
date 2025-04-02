5fb9fe55c5270bb40ad14e37c57cca885252d8b8
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-04-02 19:12:24 +0200

    Add starting file for testing speed of chrompoly algs

---
+++ Code/chrom_alg_test.py
@@ -0,0 +1,9 @@
+# Tests speed of various algorithms for calculation orbital chrom polys
+
+import solids_prep.solids_dict_prep as sdp
+
+solids = sdp.get_all_solids_dict()
+
+# TODO: finish this program
+# will run for selected solids
+# will compute a dictionary with the computed data + time
