f1758e20414d0a73df85e0d35790ec4e2b452411
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-04-02 21:41:35 +0200

    Improve md table printing to allow transforming data before output

--- Code/t_printing/md_table_printing.py
+++ Code/t_printing/md_table_printing.py
@@ -30,7 +30,7 @@ def print_solid_one_col_poly(solid_data_dict: dict, solid_category_name = "solid
 
 # prints md table given the dictionary of data, where the dictionary points from name -> [data1, ... , dataN]
 # on input we expect list of header names passed to `data_col_headrs`
-def print_solid_mult_col_data(solid_data_dict: dict, solid_category_name : str, data_col_headrs : list[str], data_col_size = DEFAULT_COL_SIZE, caption = CAPTION_PLACEHOLDER, label = "",output_type = sys.stdout):
+def print_solid_mult_col_data(solid_data_dict: dict, solid_category_name : str, data_col_headrs : list[str], data_col_size = DEFAULT_COL_SIZE, caption = CAPTION_PLACEHOLDER, label = "",output_type = sys.stdout, transform = lambda x : str(x)):
     print(caption,file=output_type)
     header_line_string = f"{TABLE_COLS_SEPARATOR} {solid_category_name:^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR}"
     header_separator_string = f"{TABLE_COLS_SEPARATOR} {TABLE_ROWS_SEPARATOR:{TABLE_ROWS_SEPARATOR}^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR}"
@@ -43,6 +43,6 @@ def print_solid_mult_col_data(solid_data_dict: dict, solid_category_name : str, 
         solid_data_cols = solid_data_dict[solid_name]
         table_entry_row = f"{TABLE_COLS_SEPARATOR} {solid_name:^{NAME_COL_SIZE}} {TABLE_COLS_SEPARATOR}"
         for solid_data_entry in solid_data_cols:
-            table_entry_row += f" {solid_data_entry:^{data_col_size}} {TABLE_COLS_SEPARATOR}"
+            table_entry_row += f" {transform(solid_data_entry):^{data_col_size}} {TABLE_COLS_SEPARATOR}"
         print(table_entry_row,file=output_type)
     print(file=output_type)
