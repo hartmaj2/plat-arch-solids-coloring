7eaf9313b40b5b4848c09ca6e6fcdc987c15cff7
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-04-09 16:00:08 +0200

    Adapt automorphisms table

--- Code/automorphisms.py
+++ Code/automorphisms.py
@@ -3,7 +3,7 @@
 # Prints a table that shows how many automorphisms the platonic and archimedean solids have
 
 import solids_prep.solids_dict_prep as sdp
-import t_printing.latex_table_printing as printing
+import t_printing.latex_table_printing as tp
 
 from sage.all import Graph
 
@@ -34,32 +34,28 @@ column_headers = LATEX_COL_HEADERS
 
 # OUTPUT FOLDER 
 ROOT_FOLDER = "Code"
-output_file = open(ROOT_FOLDER + "/Results/automorphisms.md","w")
-output_type = output_file
+# output_file = open(ROOT_FOLDER + "/Results/automorphisms.md","w")
+# output_type = output_file
+
+import sys
+output_type = sys.stdout
 
 COL_SIZE = 20
 
-print("Platonic solids automorphisms: ")
 for solid_name in platonic.keys():
     solid = platonic[solid_name]
     g = Graph(solid[EDGES])
     G = g.automorphism_group()
     order = G.order()
     plat_data[solid_name] = [order]
-    # print(f"{solid_name:30} {order}")
 
-print()
-print("Archimedean solids automorphisms: ")
 for solid_name in archimedean.keys():
     solid = archimedean[solid_name]
     g = Graph(solid[EDGES])
     G = g.automorphism_group()
     order = G.order()
     arch_data[solid_name] = [order]
-    # print(f"{solid_name:30} {G.order()}") 
 
-printing.print_solid_mult_col_data(plat_data,PLATONIC_HEADER,column_headers,PLATONIC_CAPTION,PLATONIC_LABEL,output_type)
-printing.print_solid_mult_col_data(arch_data,ARCHIMEDEAN_HEADER,column_headers,ARCHIMEDEAN_CAPTION,ARCHIMEDEAN_LABEL,output_type)
-
-# print(f"{platonic[solid_key]}")
+tp.print_solid_mult_col_data(plat_data,tp.STD_PLAT_TABLE_ORDER,PLATONIC_HEADER,column_headers,PLATONIC_CAPTION,PLATONIC_LABEL,output_type)
+tp.print_solid_mult_col_data(arch_data,tp.STD_ARCH_TABLE_ORDER,ARCHIMEDEAN_HEADER,column_headers,ARCHIMEDEAN_CAPTION,ARCHIMEDEAN_LABEL,output_type)
 
